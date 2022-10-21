b789fdce5236108103e88cb5472fa25e3a2f5cca
Rimsha Khan <116371933+Rimsha2022@users.noreply.github.com>
2022-10-21 21:09:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Shezwan rice')
+streamlit.text('Noodles')
+streamlit.text('Veg burgers')
